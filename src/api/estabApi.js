32810742d832fd4acbd55139434e110b0d492905
Lugui14 <zanelallopes9977@gmail.com>
2024-05-06 15:43:04 -0300

    refactor: prettier eslint

--- src/api/estabApi.js
+++ src/api/estabApi.js
@@ -1,7 +1,7 @@
 import api from "./axios";
 
-export const fetchEstab_by_id = async (id) => {
-    const { data } = await api.get(`/estabelecimento/${id}`);
+export const fetchEstab_by_id = async id => {
+  const { data } = await api.get(`/estabelecimento/${id}`);
 
   return data;
 };
--- src/components/GeoCoderMarker/GeoCoderMarker.jsx
+++ src/components/GeoCoderMarker/GeoCoderMarker.jsx
@@ -1,37 +1,51 @@
-import React, { useEffect, useState } from 'react'
-import { Marker, Popup, useMap } from 'react-leaflet'
-import * as ELG from "esri-leaflet-geocoder"
-import L from 'leaflet'
-import "leaflet/dist/leaflet.css"
-import icon from "leaflet/dist/images/marker-icon.png"
-import iconShadow from "leaflet/dist/images/marker-shadow.png"
-let DefaultIcon = L.icon({
-    iconUrl:icon, shadowUrl:iconShadow
-})
-L.Marker.prototype.options.icon=DefaultIcon
+import { useEffect, useState } from "react";
+import { Marker, Popup, useMap } from "react-leaflet";
+import * as ELG from "esri-leaflet-geocoder";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+import icon from "leaflet/dist/images/marker-icon.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
+import PropTypes from "prop-types";
 
-function GeoCoderMarker({address}) {
-    const map=useMap()
-    console.log(address)
-    const [position,setPosition]=useState([60,19])
-    useEffect(()=>{
-      ELG.geocode({apikey:"AAPK85b1ed2c43294675a4463b5fb9c9c350YJKLr0Y3tFNySca1WlVeoLJx397TURUTcrUHB55FagTGn8UorfyV_kHXcpEx20Da"}).text(address).run((err,results,response)=>{
-        if(results?.results?.length>0){
-            const {lat, lng} =results?.results[0].latlng
-            setPosition([lat,lng])
-            map.flyTo([lat,lng],16)
+let DefaultIcon = L.icon({
+  iconUrl: icon,
+  shadowUrl: iconShadow,
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
+
+function GeoCoderMarker({ address }) {
+  const map = useMap();
+  console.log(address);
+
+  const [position, setPosition] = useState([60, 19]);
+
+  useEffect(() => {
+    ELG.geocode({
+      apikey:
+        "AAPK85b1ed2c43294675a4463b5fb9c9c350YJKLr0Y3tFNySca1WlVeoLJx397TURUTcrUHB55FagTGn8UorfyV_kHXcpEx20Da",
+    })
+      .text(address)
+      .run((err, results) => {
+        if (results?.results?.length > 0) {
+          const { lat, lng } = results.results[0].latlng;
+          setPosition([lat, lng]);
+          map.flyTo([lat, lng], 16);
+        } else {
+          console.log("Endereço não encontrado");
         }
-        else{
-            console.log("Endereço não encontrado")
-        
-        }
-      })
-    },[address])
+      });
+  }, [address]); //eslint-disable-line
+
   return (
-    <Marker position={position} icon={DefaultIcon} >
-        <Popup/>
+    <Marker position={position} icon={DefaultIcon}>
+      <Popup />
     </Marker>
-  )
+  );
 }
 
-export default GeoCoderMarker
+export default GeoCoderMarker;
+
+GeoCoderMarker.propTypes = {
+  address: PropTypes.string,
+};
