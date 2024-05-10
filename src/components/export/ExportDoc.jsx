de2bd271b5d4d078b648a5ea24b2413923a08487
Vinicius dos Reis de Jesus <vinicius.jesus@amo.delivery>
2024-05-10 11:15:53 -0300

    feat: add download

--- src/components/export/ExportDoc.jsx
+++ src/components/export/ExportDoc.jsx
@@ -1,11 +1,35 @@
-import { Button, Grid } from "@mui/material";
-import { FaRegFilePdf, FaRegFileExcel } from "react-icons/fa6";
+import { Button, Grid } from "@mui/material"
+import { FaRegFilePdf, FaRegFileExcel } from "react-icons/fa6"
+import axios from 'axios'
+
+const BASE_URL = 'http://localhost:8000'
+
+const handleDownload = async fileName => {
+  try {
+    const response = await axios.get(`${BASE_URL}/estabelecimento/${fileName}/download`, {
+        responseType: 'blob'
+    })
+
+    const url = window.URL.createObjectURL(new Blob([response.data]))
+    
+    const link = document.createElement('a')
+    link.href = url
+    link.download = fileName
+
+    document.body.appendChild(link)
+    link.click()
+
+    document.body.removeChild(link)
+  } catch (error) {
+    console.error('Erro durante o download:', error)
+  }
+};
 
 const ExportDoc = () => {
   return (
     <Grid container spacing={10}>
       <Grid item xs={12} sm={6}>
-        <Button onClick={''} style={{ width: '100%' }}>
+        <Button onClick={() => handleDownload('estabelecimentos.pdf')} style={{ width: '100%' }}>
           <Grid container alignItems="center">
             <Grid item>
               <FaRegFilePdf style={{ marginRight: '8px', fontSize: '100px', color: 'red' }} />
@@ -15,7 +39,7 @@ const ExportDoc = () => {
       </Grid>
 
       <Grid item xs={12} sm={6}>
-        <Button onClick={''} style={{ width: '100%' }}>
+        <Button onClick={() => handleDownload('estabelecimentos.xlsx')} style={{ width: '100%' }}>
           <Grid container alignItems="center">
             <Grid item>
               <FaRegFileExcel style={{ marginRight: '8px', fontSize: '100px', color: 'green' }} />
